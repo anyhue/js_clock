3c9b078801754ad3bcea27c8309e7f86626052a1
anyhue <111332914+anyhue@users.noreply.github.com>
2022-08-27 16:37:53 +0900

    js_clock
    interval

---
+++ clock.js
@@ -0,0 +1,9 @@
+const clock = document.querySelector("#clock");
+
+function getClock() {
+    const date = new Date();
+    clock.innerText = (`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+}
+
+getClock(); //시계 바로 호출
+setInterval(getClock, 1000); 
